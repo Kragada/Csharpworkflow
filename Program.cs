9b9cabb24a98f4272494070ddc1eed5aa44ebfb9
Kragada <35291972+Kragada@users.noreply.github.com>
2021-09-12 22:51:35 +0200

    commit 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Csharpworkflow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello My World!");
         }
     }
 }
